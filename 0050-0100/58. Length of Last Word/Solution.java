571520523fae90b004affee84a3156a5b44e9e88
Monisha S <smsmmitha@gmail.com>
2024-05-29 08:13:21 +0530

    Created Solution.java
    Using iterations

---
+++ 0050-0100/58. Length of Last Word/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i = s.length()-1 ; i>=0;i--){
+            if(s.charAt(i) != ' ')
+                count++;
+                         //Break for Last Word
+            if(count > 0  && s.charAt(i) == ' ' )
+                break;
+        }
+        return count;
+    }
+}
